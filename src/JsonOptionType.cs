241a8314a05714b52d53d48751753e26e996ddd2
Jake Soenneker <jake@soenneker.com>
2024-11-18 08:50:24 -0600

    Removal of Ardalis

--- src/JsonOptionType.cs
+++ src/JsonOptionType.cs
@@ -1,32 +1,29 @@
-﻿using Ardalis.SmartEnum;
+﻿namespace Soenneker.Enums.JsonOptions;
 
-namespace Soenneker.Enums.JsonOptions;
-
-public sealed class JsonOptionType : SmartEnum<JsonOptionType>
+/// <summary>
+/// Represents different JSON option types.
+/// </summary>
+public enum JsonOptionType
 {
     /// <summary>
     /// Web defaults, non-strict
     /// </summary>
-    public static readonly JsonOptionType Web = new(nameof(Web), 0);
+    Web = 0,
 
     /// <summary>
     /// Non-camel case
     /// </summary>
-    public static readonly JsonOptionType General = new(nameof(General), 1);
+    General = 1,
 
     /// <summary>
     /// Non-camel case with indentation WITHOUT escaping. WARNING Dangerous! Do not use unless for internal uses!
     /// </summary>
     /// <remarks>https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-character-encoding</remarks>
-    public static readonly JsonOptionType Pretty = new(nameof(Pretty), 2);
+    Pretty = 2,
 
     /// <summary>
     /// Non-camel case with indentation, WITH escaping. Safe for output.
     /// </summary>
     /// <remarks>https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-character-encoding</remarks>
-    public static readonly JsonOptionType PrettySafe = new(nameof(PrettySafe), 3);
-
-    private JsonOptionType(string name, int value) : base(name, value)
-    {
-    }
+    PrettySafe = 3
 }
