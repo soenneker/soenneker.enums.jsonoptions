e3c1c41d751c9818c5b01b2ef9982460e8a2c341
Jake Soenneker <jake@soeneker.com>
2024-12-22 14:01:49 +0000

    Tests

---
+++ test/Soenneker.Enums.JsonOptions.Tests/JsonOptionTypeTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Enums.JsonOptions.Tests;
+
+[Collection("Collection")]
+public class JsonOptionTypeTests : FixturedUnitTest
+{
+    public JsonOptionTypeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
