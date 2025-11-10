f2269aae379a83711b7934f8b4fdd17b5dd9ab6f
Jake Soenneker <jake@soeneker.com>
2025-11-09 19:19:02 -0600

    sealing

--- src/JsonOptionType.cs
+++ src/JsonOptionType.cs
@@ -1,29 +1,32 @@
-﻿namespace Soenneker.Enums.JsonOptions;
+﻿using Intellenum;
+
+namespace Soenneker.Enums.JsonOptions;
 
 /// <summary>
 /// Represents different JSON option types.
 /// </summary>
-public enum JsonOptionType
+[Intellenum<int>]
+public sealed partial class JsonOptionType
 {
     /// <summary>
     /// Web defaults, non-strict
     /// </summary>
-    Web = 0,
+    public static readonly JsonOptionType Web = new(0);
 
     /// <summary>
     /// Non-camel case
     /// </summary>
-    General = 1,
+    public static readonly JsonOptionType General = new(1);
 
     /// <summary>
     /// Non-camel case with indentation WITHOUT escaping. WARNING Dangerous! Do not use unless for internal uses!
     /// </summary>
     /// <remarks>https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-character-encoding</remarks>
-    Pretty = 2,
+    public static readonly JsonOptionType Pretty = new(2);
 
     /// <summary>
     /// Non-camel case with indentation, WITH escaping. Safe for output.
     /// </summary>
     /// <remarks>https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-character-encoding</remarks>
-    PrettySafe = 3
+    public static readonly JsonOptionType PrettySafe = new(3);
 }
